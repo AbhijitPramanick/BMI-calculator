9bf7a4f119e80ad47d9a1f7f79d27dbc097ce908
AbhijitPramanick <amanik1995@gmail.com>
2024-01-20 12:00:51 +0530

    Display div with background blur implemented

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="master">
+      <h1 className="heading">BMI Calculator</h1>
+    </div>
+  );
+};
+
+export default App;
